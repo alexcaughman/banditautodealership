0ed7107b17131ffcd4394d284a7eef17d00c92e6
jimlee4262 <jimlee4262@gmail.com>
2020-09-24 09:47:17 -0400

    Added graph labels and comments

--- load-csv/chart.js
+++ load-csv/chart.js
@@ -1,34 +1,54 @@
 function makeChart(cars){
-    var carslabels = cars.map(function(d){
+    // car labels its own array
+    var carlabels = cars.map(function(d){
         return d.Make;
     });
+    // car price in its own array
     var carprice = cars.map(function(d){
         return +d.price;
     });
+    // car msrp in its own array
     var carmsrp = cars.map(function(d){
-        return +d.MSRP
+        return +d.MSRP;
     })
 
-    console.log(carprice)
-    console.log(carmsrp)
-    console.log(carslabels)
+    // car year in itts own array
+    var caryear = cars.map(function(d){
+        return +d.Year
 
+    })
+
+    // loop carmodel in an array as the map function doesn't work
+    carmodel=[]
+    for(var i = 0; i <cars.length; i++){
+        carmodel.push(cars[i].Model)
+    }
+
+    // combinining year, carlabel and model together
+    carlabelmodel = []
+    for(var i = 0; i < carmodel.length; i++){
+        carlabelmodel.push(caryear[i]+" " +carlabels[i]+" - "+carmodel[i])
+    }
+
+    // creating chartjs horizontal bar chart
     var chart = new Chart('chart', {
         type: "horizontalBar",
         options:{
-            maintainAspectRatio: false,
+            maintainAspectRatio: true,
             legend:{
-                display: false
+                display: true
             }
         },
         data: {
-            labels: carslabels,
+            labels: carlabelmodel,
             datasets:[
                 {
+                    label: "Car Price on Craigslist",
                     data: carprice,
                     backgroundColor: "#3e95cd"
                 },
                 {
+                    label: "MSRP",
                     data: carmsrp,
                     backgroundColor: "#8e5ea2"
 
